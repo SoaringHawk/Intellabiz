f4e0b31bb15f3b87eb9d9693949b58715a843c69
SoaringHawk <christym131@hotmail.com>
2024-11-22 20:58:49 -0500

    added media directory

--- Intellabiz/settings.py
+++ Intellabiz/settings.py
@@ -126,6 +126,9 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
